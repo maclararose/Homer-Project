caad801c37b8410d79d0b8c3dcdd7b3325340a5b
Maria Clara <mcbarroso.contato@gmail.com>
2021-06-01 23:02:40 -0300

    First commit for front end project

---
+++ plataform_front_end/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react';
+import Home from './components/home/index'
+
+function App() {
+  return (
+    <div className="container">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ plataform_front_end/src/components/home/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./home.css";
+
+function Home(){
+  return(
+    <div className="container">
+      <div className="box-1"></div>
+      <div className="box-2"></div>
+      <div className="box-3"></div>
+      <div className="box-4"></div>
+    </div>
+  );
+}
+
+export default Home;
