ea60cb0a0a6e413db51d186ef28f40167258511b
Quemia <que.carol@hotmail.com>
2021-06-10 11:18:29 -0300

    Excluir Comentário

--- plataform_front_end/src/components/home/index.js
+++ plataform_front_end/src/components/home/index.js
@@ -7,17 +7,6 @@ function Home(){
     <div className="container">
 
     </div>
-    /*
-    <div className="container">
-      <div className="box-1"></div>
-      <div className="box-2"></div>
-      <div className="box-3"></div>
-      <div className="box-4"></div>
-      <button>open</button>
-    </div>
-    */
-   
-  //  ReactDOM.render(loader, document.getElementById('root'));
     );
 }
 
